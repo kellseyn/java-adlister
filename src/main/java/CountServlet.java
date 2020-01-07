0c6e9f51f650219e48c82eabb42b4ff55d0dde77
Kellsey Neagley <kellseyneagley@gmail.com>
2020-01-07 14:00:02 -0600

    completed servlets exercise

--- src/main/java/CountServlet.java
+++ src/main/java/CountServlet.java
@@ -3,19 +3,18 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
-
-
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
-public class HelloWorldServlet extends HttpServlet {
-
+@WebServlet(name = "CountServlet", urlPatterns = "/count")
+public class CountServlet extends HttpServlet {
+    int count = 0;
 
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        out.println("<h1>Hello, World!</h1>");
+        out.println("<h1>" + (count++) + "</h1>");
+        String count = request.getParameter("count");
+
     }
 
 }
-
